1ca87a61c80f50fffee3d69ade3de2d1b50120d7
Shalu004 <mushaluthakur785@gmail.com>
2025-09-17 19:48:46 +0000

    changes add

--- week2/create_http_server.js
+++ week2/create_http_server.js
@@ -12,3 +12,25 @@ app.get("/", function(req, res){
    res.send("hi there");
 })
 app.listen(3000);
+
+//ANOTHER EXAMPLE
+/*
+const express = require("express");
+
+function calculateSum(n){
+    let ans=0;
+    for(let i=0;i<=n;i++){
+        ans=ans+i;
+    }
+    return ans;
+}
+
+const app = express();
+
+app.get("/", (req, res) {
+    const n = req.query.n;
+    const ans= calculateSum(n);
+    res.send(ans.toString());
+});
+app.listen(3000);
+*/
