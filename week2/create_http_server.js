1f90c0152733a55b0fa0f1e8dc6eaac35e4c1b4c
Shalu004 <mushaluthakur785@gmail.com>
2025-09-17 19:54:30 +0000

    added

--- week2/create_http_server.js
+++ week2/create_http_server.js
@@ -33,4 +33,12 @@ app.get("/", (req, res) {
     res.send(ans.toString());
 });
 app.listen(3000);
+
+
+Request methods
+1. GET - Going for a consultation to get a check up
+2. POST - Going to get a new kidney inserted
+3. PUT - Going to get a kidney replaced
+4. DELETE - Going to get a kidney removed
+
 */
